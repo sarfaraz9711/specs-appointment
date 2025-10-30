1eb9674e478da0bba759e0fb1d03842acdb1ea5c
sarfaraz9711 <57387817+sarfaraz9711@users.noreply.github.com>
2025-10-30 13:05:17 +0530

    Update camera.jsx

--- pages/appointment/camera.jsx
+++ pages/appointment/camera.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import { FaceMesh } from "@mediapipe/face_mesh";
 import { Camera } from "@mediapipe/camera_utils";
 
 const availableSpecs = [
@@ -28,13 +27,16 @@ const VirtualSpecsTryOn = () => {
   // FaceMesh setup
   useEffect(() => {
     if (!videoRef.current || !canvasRef.current) return;
-
+    if (typeof window !== "undefined") {
+      const { FaceMesh } = require("@mediapipe/face_mesh");
+    
     const faceMesh = new FaceMesh({
       locateFile: (file) =>
         `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`,
     });
 
     faceMesh.setOptions({
+      selfieMode: true,
       maxNumFaces: 1,
       refineLandmarks: true,
       minDetectionConfidence: 0.5,
@@ -53,7 +55,7 @@ const VirtualSpecsTryOn = () => {
         drawSpecs(ctx, landmarks, glassesImgRef.current);
       }
     });
-
+  
     const camera = new Camera(videoRef.current, {
       onFrame: async () => {
         await faceMesh.send({ image: videoRef.current });
@@ -61,9 +63,10 @@ const VirtualSpecsTryOn = () => {
       width: 640,
       height: 480,
     });
-
+  
     camera.start();
     return () => camera.stop();
+  }
   }, []);
 
   const drawSpecs = (ctx, landmarks, img) => {
